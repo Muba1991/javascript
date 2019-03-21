016d2ad9fddc4e4829a772a04022a95e70a77229
Muba1991 <sidauruk.1991@gmail.co.id>
2019-03-21 21:49:07 +0700

    Funtion

---
+++ funtion.js
@@ -0,0 +1,15 @@
+function jalansatu(){
+  console.log("Jalan Satu")
+}
+
+const jalandua = function(){
+  console.log("Jalan Dua")
+}
+
+jalansatu()
+jalandua()
+
+const jalantiga = () =>{
+  console.log("Jalan Tiga")
+}
+jalantiga()
---
+++ index.js
@@ -0,0 +1,12 @@
+var message = "Hello Coding";
+let msg1 = "Hello Coding Java";
+
+const msg2 = "Coding dengan java";
+
+if(true){
+  var message = 1;
+  let msg1 = 2;
+  console.log("testing", msg1)
+}
+console.log(message)
+console.log(msg1)
